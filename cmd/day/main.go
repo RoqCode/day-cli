5e9e6aa6e3f5847a6c9c3702023de9311aeb32f1
Hajo Haas <hajo.haas@hmmh.de>
2026-02-21 15:55:21 +0100

    test: add small end to end test for db integration

--- cmd/day/main.go
+++ cmd/day/main.go
@@ -1,7 +1,56 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/roqcode/day/internal/db"
+)
 
 func main() {
-	fmt.Println("hallo day")
+	database, err := db.InitDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer database.Close()
+
+	pings := []db.Ping{
+		{
+			TS:       time.Now(),
+			Activity: "commit auf branch: MP-1111",
+			Scope:    "MP-1111",
+			Source:   "gc",
+		},
+		{
+			TS:       time.Now(),
+			Activity: "Refinement",
+			Scope:    "Scrum Events",
+			Source:   "manual",
+		},
+		{
+			TS:       time.Now(),
+			Activity: "branch wechsel: MP-1111 -> origin/develop",
+			Scope:    "MP-1111",
+			Source:   "gs",
+		},
+	}
+
+	for _, ping := range pings {
+		if err := database.InsertPing(ping); err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println("Ping geschrieben.")
+	}
+
+	pingsRes, err := database.GetPingsForDay(time.Now())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("\nPings heute (%d):\n", len(pingsRes))
+	for _, p := range pingsRes {
+		fmt.Printf("  [%s] %s (scope: %s, source: %s)\n",
+			p.TS.Format("15:04:05"), p.Activity, p.Scope, p.Source)
+	}
 }
